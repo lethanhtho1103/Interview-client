5f1dec8328d4df346f9be2f4dcd5d9cb58c0af7f
Thanh Tho <letanhtho1953@gmail.com>
2024-10-19 11:13:24 +0700

    Kiem tra dieu kien xem nguoi dung da dang nhap chua

--- app.js
+++ app.js
@@ -5,10 +5,24 @@ app.config(function ($routeProvider) {
     .when("/home", {
       templateUrl: "app/views/home.html",
       controller: "HomeController",
+      resolve: {
+        auth: function (AuthService, $location) {
+          if (!AuthService.isLoggedIn()) {
+            $location.path("/login");
+          }
+        },
+      },
     })
     .when("/login", {
       templateUrl: "app/views/login.html",
       controller: "LoginController",
+      resolve: {
+        auth: function (AuthService, $location) {
+          if (AuthService.isLoggedIn()) {
+            $location.path("/home");
+          }
+        },
+      },
     })
     .otherwise({
       redirectTo: "/login",
